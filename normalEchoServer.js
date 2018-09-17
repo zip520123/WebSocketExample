48c42952850b533ff3c25c4135a74f41dda7c914
zip520123 <zip520123@yahoo.com.tw>
2018-09-17 09:38:52 +0800

    add normal Echo server

---
+++ normalEchoServer.js
@@ -0,0 +1,8 @@
+const net = require('net')
+
+net.createServer(socket => {
+  socket.on('data', function(data){
+    console.log('Echoing: %s', data.toString())
+    socket.write(data.toString())
+  })
+}).listen(8080)
