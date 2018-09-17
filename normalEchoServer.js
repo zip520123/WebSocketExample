4211b33a3b763a79becda295515600573fd40f46
zip520123 <zip520123@yahoo.com.tw>
2018-09-17 09:49:29 +0800

    echo to raw data

--- normalEchoServer.js
+++ normalEchoServer.js
@@ -3,6 +3,7 @@ const net = require('net')
 net.createServer(socket => {
   socket.on('data', function(data){
     console.log('Echoing: %s', data.toString())
-    socket.write(data.toString())
+    socket.write(data);
+//    socket.write(data.toString());
   })
 }).listen(8080)
